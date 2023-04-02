404b8a472003a10072aba49295b6d549e34dd3a2
Ieuan Walker <ieuan.walker007@gmail.com>
2023-04-02 15:40:05 +0100

    Update

--- Scr/CustomSwitch/Handler/SwitchViewHandler.android.cs
+++ Scr/CustomSwitch/Handler/SwitchViewHandler.android.cs
@@ -1,12 +1,27 @@
 ﻿using System.Diagnostics;
+using Android.Media.Midi;
 using Microsoft.Maui.Handlers;
-using Microsoft.Maui.Platform;
 
 namespace CustomSwitch.Handler;
 public partial class SwitchViewHandler : ContentViewHandler, ISwitchViewHandler
 {
-	public static void MapIsToggled(ISwitchViewHandler handler, IContentView switchView)
+	protected override CustomContentViewGroup CreatePlatformView()
 	{
+		if (VirtualView == null)
+		{
+			throw new InvalidOperationException($"{nameof(VirtualView)} must be set to create a ContentViewGroup");
+		}
 
+		var viewGroup = new CustomContentViewGroup(Context, this.VirtualView);
+		viewGroup.SetClipChildren(false);
+
+		return viewGroup;
+	}
+	public static void MapIsToggled(ISwitchViewHandler handler, IContentView contentView)
+	{
+		if (handler.PlatformView is CustomContentViewGroup platformView && contentView is ISwitchView switchView)
+		{
+			platformView.SetIsToggled(switchView.IsToggled);
+		}
 	}
 }
