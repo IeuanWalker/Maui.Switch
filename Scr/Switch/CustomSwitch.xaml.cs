5847e776101feb1e13cb27d02e94cf2d1706446e
Ieuan Walker <ieuan.walker007@gmail.com>
2024-11-19 19:02:59 +0000

    Update target frameworks and reorganize project files
    Updated target frameworks in `App.csproj` and `IeuanWalker.Maui.Switch.csproj` from .NET 8.0 to .NET 9.0. Removed `net8.0-maccatalyst` target framework from `App.csproj`. Reorganized `App.csproj` with moved and restructured properties and item groups. Removed `SupportedOSPlatformVersion` for `maccatalyst`, `windows`, and `tizen` from `App.csproj`. Removed `CodesignKey`, `CodesignProvision`, and `ProvisioningType` properties for `net8.0-ios` from `App.csproj`. Restructured several `ItemGroup` elements in `App.csproj`, including resources, fonts, images, and package references. Updated `PackageReleaseNotes` in `IeuanWalker.Maui.Switch.csproj`. Updated `ItemGroup` conditions in `IeuanWalker.Maui.Switch.csproj` to reflect new target frameworks. Changed `BindableProperty` default values in `CustomSwitch.xaml.cs` from `Rectangle` to `Rect`. Removed leading BOM character from XML declaration in several XAML files.

--- Scr/Switch/CustomSwitch.xaml.cs
+++ Scr/Switch/CustomSwitch.xaml.cs
@@ -40,7 +40,7 @@ public partial class CustomSwitch : SwitchView
 		set => SetValue(WidthRequestProperty, value);
 	}
 
-	public static readonly BindableProperty StrokeShapeProperty = BindableProperty.Create(nameof(StrokeShape), typeof(IShape), typeof(CustomSwitch), new Rectangle());
+	public static readonly BindableProperty StrokeShapeProperty = BindableProperty.Create(nameof(StrokeShape), typeof(IShape), typeof(CustomSwitch), new Rect());
 
 	[TypeConverter(typeof(StrokeShapeTypeConverter))]
 	public IShape? StrokeShape
@@ -126,7 +126,7 @@ public partial class CustomSwitch : SwitchView
 		set => SetValue(KnobBackgroundColorProperty, value);
 	}
 
-	public static readonly BindableProperty KnobStrokeShapeProperty = BindableProperty.Create(nameof(KnobStrokeShape), typeof(IShape), typeof(CustomSwitch), new Rectangle());
+	public static readonly BindableProperty KnobStrokeShapeProperty = BindableProperty.Create(nameof(KnobStrokeShape), typeof(IShape), typeof(CustomSwitch), new Rect());
 
 	[TypeConverter(typeof(StrokeShapeTypeConverter))]
 	public IShape? KnobStrokeShape
