5a47f852cdc4fac2c74da4f8c3b8d504ead940e4
Ieuan Walker <ieuan.walker007@gmail.com>
2024-11-19 19:15:47 +0000

    Refactor App initialization to use CreateWindow method
    Modified the App constructor to remove direct initialization of MainPage with AppShell. Added a new CreateWindow method that overrides the base class method to create and return a new Window object initialized with AppShell. This change improves the structure and initialization process of the application.

--- Demo/App/App.xaml.cs
+++ Demo/App/App.xaml.cs
@@ -5,7 +5,6 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new AppShell();
 	}
+	protected override Window CreateWindow(IActivationState? activationState) => new(new AppShell());
 }
